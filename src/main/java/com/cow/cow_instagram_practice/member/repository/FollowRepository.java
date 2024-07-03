25911a5c50df16ce5d8fce5d92c8f682d4e80ad8
BuildTools <unconfigured@null.spigotmc.org>
2024-07-03 18:34:06 +0900

    feat : 팔로우 repository 메서드 추가

--- src/main/java/com/cow/cow_instagram_practice/member/repository/FollowRepository.java
+++ src/main/java/com/cow/cow_instagram_practice/member/repository/FollowRepository.java
@@ -1,8 +1,12 @@
 package com.cow.cow_instagram_practice.member.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cow.cow_instagram_practice.member.entity.Follow;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+	List<Follow> findByFollowerId(String follower);
+	List<Follow> findByFollowingId(String following);
 }
