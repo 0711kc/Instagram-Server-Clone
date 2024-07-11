3df4454e32956c35287b890031ee2469ab64cfdb
BuildTools <unconfigured@null.spigotmc.org>
2024-07-12 01:25:01 +0900

    chore : Update gradle.yml

--- src/test/java/com/cow/cow_instagram_practice/CowInstagramPracticeApplicationTests.java
+++ src/test/java/com/cow/cow_instagram_practice/CowInstagramPracticeApplicationTests.java
@@ -3,7 +3,7 @@ package com.cow.cow_instagram_practice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CowInstagramPracticeApplicationTests.class)
 class CowInstagramPracticeApplicationTests {
 
 	@Test
